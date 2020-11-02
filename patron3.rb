ce58e235eaa68d7ebcf8084a4e45e1244a4355f1
Ale Fuentes <jano231982@gmail.com>
2020-11-02 14:59:18 -0300

    patron3 ok

--- patron3.rb
+++ patron3.rb
@@ -1,9 +1,11 @@
 veces = ARGV[0].to_i 
 
 veces.times do |i|
-   if i%2 == 0 
+   if i%3 == 0 
     print '1'
-   else 
-    print "2"    
+   elsif i%3 == 1 
+    print "2"
+   elsif i%3 == 2
+    print "3"    
    end
 end
